df71ca6e2ddd615d92fce579097be59755bd310f
Matt Canavan <mattcanavan@users.noreply.github.com>
2020-10-02 10:18:28 -0700

    Tests completed. Basically MVP.

--- client/src/components/CheckoutForm.test.js
+++ client/src/components/CheckoutForm.test.js
@@ -7,10 +7,34 @@ import CheckoutForm from "./CheckoutForm";
 test("form header renders", () => {
     //arrange
     render(<CheckoutForm />);
-    const formHeader = screen.getByText(/checkout form /i)
+    const formHeader = screen.getByText(/checkout form/i)
 
-    //act
+    //assert
+    expect(formHeader).toBeInTheDocument()
 
 });
 
-test("form shows success message on submit with form details", () => {});
+test("form shows success message on submit with form details", () => {
+    //arrange
+    render(<CheckoutForm />);
+    const firstName = screen.getByLabelText(/first name/i);
+    const lastName = screen.getByLabelText(/last name/i);
+    const address = screen.getByLabelText(/address/i);
+    const city = screen.getByLabelText(/city/i);
+    const usaState = screen.getByLabelText(/state/i);
+    const zipCode = screen.getByLabelText(/zip/i);
+    const checkoutBtn = screen.getByRole('button', { name: /checkout/i});
+    
+    //act
+    fireEvent.change(firstName, { target: { value: 'Andre'}});
+    fireEvent.change(lastName, { target: { value: '3000'}});
+    fireEvent.change(address, { target: { value: '420 Rainbow Road'}});
+    fireEvent.change(city, { target: { value: 'ATL'}});
+    fireEvent.change(usaState, { target: { value: 'Georgia'}});
+    fireEvent.change(zipCode, { target: { value: '30301'}});
+    fireEvent.click(checkoutBtn)
+
+    //assert
+    expect(screen.getByText(/You have ordered some plants! Woo-hoo!/i)).toBeInTheDocument()
+
+});
